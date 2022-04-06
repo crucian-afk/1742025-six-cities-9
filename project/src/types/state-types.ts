3fd1e4f70b33c7dbfda1361eb5d3b3de5bdc27bd
Юрий <y.headman@ya.ru>
2022-04-06 22:01:00 +0300

    добавляет редукс и перерабатывает компоненты

--- project/src/types/state-types.ts
+++ project/src/types/state-types.ts
@@ -1,9 +1,4 @@
 import {store} from '../store';
 
-// export type State = {
-//   city: string;
-//   offers: OfferTypes[];
-// }
 export type State = ReturnType<typeof store.getState>;
-
 export type AppDispatch = typeof store.dispatch;
